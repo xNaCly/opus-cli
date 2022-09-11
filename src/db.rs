a202ac6a56eadc5555c7ce29640e8d3036991478
xnacly <47723417+xNaCly@users.noreply.github.com>
2022-09-11 16:08:04 +0200

    doc: thinking about switching from file to sqlite
    using a file as a database has some advantages for the user but a lot of disatvantages for the developer.

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,24 @@
-//! Opus database wrapper
+// todo: Thinking about the implementation of a database:
+//! # Thinking about the implementation of a database:
+//!
+//! maybe switch from a file based approch to a sqlite database, the currently planned implementation has the following flaws, which could be removed by using a relational database instead of a file:
+//!
+//! - deleting a row would cause the program to read the whole file, remove the row and write the whole file minus the row back to disk (this is a waste of ressources). Using sql one can just `DELETE` an entry
+//!
+//! - to keep tabs on the current id, the program needs to read all lines from the disk and increment the last id. Sqlite autoincrements the primary id.
+//!
+//! - `opus list` requires a query engine, which is already included in sql, but not in this program
+//!
+//!
+//! # Opus database wrapper
 //!
 //! Opus follows the unix principle and therefore stores all its data in a text based csv database.
+//!
+//! Opus config file contains a header in the first row specifying all columns:
+//!
+//! ```csv
+//! id;title;tag;dueDate;finished
+//! ```
 use crate::types::Task;
 use std::env::consts::OS;
 use std::env::var;
@@ -23,10 +41,12 @@ const CONFIG_PATH: &str = "/opus/opus_todo.txt";
 /// - linux ❌
 /// - macos ❌
 pub fn get_db_path() -> String {
+    // todo: uncomment this after testing
     // let opus_path = match var("OPUS_PATH") {
     //     Ok(r) => r,
     //     Err(e) => "".to_string(),
     // };
+    // todo: remove this after testing
     let opus_path = "c:/Users/gro112741/Desktop/opus_todo.txt".to_string();
 
     if !opus_path.is_empty() {
@@ -67,8 +87,10 @@ pub fn db_add(t: Task) {
     if !does_file_exist(&path) {
         panic!("Can't find database file")
     }
+    // todo: implement writing task to database
 }
 pub fn db_remove(id: usize) {
+    // todo: implement removing a task by clearing the row
     unimplemented!()
 }
 pub fn db_finish(id: usize) {
@@ -84,6 +106,7 @@ pub fn db_get() {
     let file = File::open(path).expect("Couldn't open database file");
     let reader = BufReader::new(file);
 
+    // todo: implement parsing line into task struct
     for line in reader.lines() {
         println!("{}", line.unwrap());
     }
