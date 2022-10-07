ee6020ea10de8f043f85fd4602045e08ffb482e5
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2022-10-07 02:31:17 +0200

    lint: cargo fmt

--- src/tests.rs
+++ src/tests.rs
@@ -44,21 +44,44 @@ mod cli {
 
     #[test]
     fn parse_export() {
-
         let inputs = [
-            vec!["opus".to_owned(), "export".to_owned(), "csv".to_owned(), "tmp1".to_owned()],
-            vec!["opus".to_owned(), "export".to_owned(), "tsv".to_owned(), "tmp2".to_owned()],
-            vec!["opus".to_owned(), "export".to_owned(), "json".to_owned(), "tmp3".to_owned()]
+            vec![
+                "opus".to_owned(),
+                "export".to_owned(),
+                "csv".to_owned(),
+                "tmp1".to_owned(),
+            ],
+            vec![
+                "opus".to_owned(),
+                "export".to_owned(),
+                "tsv".to_owned(),
+                "tmp2".to_owned(),
+            ],
+            vec![
+                "opus".to_owned(),
+                "export".to_owned(),
+                "json".to_owned(),
+                "tmp3".to_owned(),
+            ],
         ];
 
         let expected = [
-            ArgumentType::Export { export_type: crate::types::ExportType::Csv, file_name: "tmp1".to_owned() },
-            ArgumentType::Export { export_type: crate::types::ExportType::Tsv, file_name: "tmp2".to_owned() },
-            ArgumentType::Export { export_type: crate::types::ExportType::Json, file_name: "tmp3".to_owned() },
+            ArgumentType::Export {
+                export_type: crate::types::ExportType::Csv,
+                file_name: "tmp1".to_owned(),
+            },
+            ArgumentType::Export {
+                export_type: crate::types::ExportType::Tsv,
+                file_name: "tmp2".to_owned(),
+            },
+            ArgumentType::Export {
+                export_type: crate::types::ExportType::Json,
+                file_name: "tmp3".to_owned(),
+            },
         ];
 
         for (input, expected) in inputs.iter().zip(expected) {
-            assert_eq!(parse_args(input.to_owned()).top_level_arg, expected)   
+            assert_eq!(parse_args(input.to_owned()).top_level_arg, expected)
         }
     }
 
@@ -72,7 +95,7 @@ mod cli {
     fn not_enough_arguments_ii() {
         parse_args(vec!["opus".to_string(), "add".to_string()]);
     }
-    
+
     #[test]
     #[should_panic]
     fn not_enough_arguments_export_1() {
@@ -82,13 +105,21 @@ mod cli {
     #[test]
     #[should_panic]
     fn not_enough_arguments_export_2() {
-        parse_args(vec!["opus".to_string(), "export".to_string(), "csv".to_owned()]);
+        parse_args(vec![
+            "opus".to_string(),
+            "export".to_string(),
+            "csv".to_owned(),
+        ]);
     }
-    
+
     #[test]
     #[should_panic]
     fn not_enough_arguments_export_3() {
-        parse_args(vec!["opus".to_string(), "export".to_string(), "file".to_owned()]);
+        parse_args(vec![
+            "opus".to_string(),
+            "export".to_string(),
+            "file".to_owned(),
+        ]);
     }
 
     #[test]
@@ -215,7 +246,14 @@ mod cli {
         let output = db.export(&crate::types::ExportType::Csv);
         assert_eq!(output, "");
 
-        db.insert_task(Task{ id: Some(1_usize), title: "title".to_owned(), tag: "tag".to_owned(), priority: 2_usize, due: "due".to_owned(), finished: false });
+        db.insert_task(Task {
+            id: Some(1_usize),
+            title: "title".to_owned(),
+            tag: "tag".to_owned(),
+            priority: 2_usize,
+            due: "due".to_owned(),
+            finished: false,
+        });
 
         let output = db.export(&crate::types::ExportType::Csv);
         assert_eq!(output, "title,tag,2,due,false\n");
